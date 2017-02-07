bcef6b0a11ef0e613f10ec1d685de76b3c1363bf
ed <ed@192.168.1.79>
2017-02-07 17:30:44 -0600

    se agrego metodo para consultar los pagos de un alumno

--- sistemapagosmodel/src/main/java/mx/com/mentoringit/model/dao/IPayment.java
+++ sistemapagosmodel/src/main/java/mx/com/mentoringit/model/dao/IPayment.java
@@ -1,8 +1,11 @@
 package mx.com.mentoringit.model.dao;
 
+import java.util.List;
+
 import mx.com.mentoringit.model.dto.PaymentDTO;
 
 public interface IPayment {
 	public void insertPayment(PaymentDTO payment) throws Exception; 
+	public List<PaymentDTO> selectPayment(Integer idStudent, Integer idProduct) throws Exception;
 
 }
--- sistemapagosmodel/src/main/java/mx/com/mentoringit/model/dao/PaymentDAO.java
+++ sistemapagosmodel/src/main/java/mx/com/mentoringit/model/dao/PaymentDAO.java
@@ -1,14 +1,20 @@
 package mx.com.mentoringit.model.dao;
 
 import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import mx.com.mentoringit.model.dto.CourseDTO;
 import mx.com.mentoringit.model.dto.PaymentDTO;
+import mx.com.mentoringit.model.dto.StudentDTO;
 
 @Repository
 public class PaymentDAO implements IPayment{
@@ -33,5 +39,32 @@ public class PaymentDAO implements IPayment{
 	
 		
 	}
+	
+	public List<PaymentDTO> selectPayment(Integer idStudent, Integer idProduct ) throws Exception {
+		String select = "select * from tbl_payment as p where p.student_id = ? and p.product_id = ?";
+		
+		List<PaymentDTO> payment = this.jdbcTemplate.query(select, new Object[]{idStudent,idProduct}, new RowMapper<PaymentDTO>(){
+
+			public PaymentDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+				PaymentDTO paymentDTO = new PaymentDTO();
+				
+				paymentDTO.setId(rs.getInt("ID"));
+				paymentDTO.setStudent_id(rs.getInt("STUDENT_ID"));
+				paymentDTO.setCourse_id(rs.getInt("COURSE_ID"));
+				paymentDTO.setNum_payment(rs.getInt("NUM_PAYMENT"));
+				paymentDTO.setAmount_payment(rs.getDouble("AMOUNT_PAYMENT"));
+				paymentDTO.setType_payment(rs.getString("TYPE_PAYMENT"));
+				paymentDTO.setDate_payment(rs.getDate("DATE_PAYMENT"));
+				paymentDTO.setTotal_course(rs.getDouble("TOTAL_COURSE"));
+				paymentDTO.setProduct_id(rs.getInt("PRODUCT_ID"));				
+				
+				return paymentDTO;
+			}
+		});		
+				
+		return payment;
+	}
+
+
 	
 }
