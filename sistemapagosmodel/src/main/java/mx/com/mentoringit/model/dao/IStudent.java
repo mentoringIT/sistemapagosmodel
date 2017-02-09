336e4f0c6723c501ca7679bf8fef6c1b71ab82a5
ed <ed@192.168.1.70>
2017-02-08 23:41:30 -0600

    nuevas consultas

--- sistemapagosmodel/src/main/java/mx/com/mentoringit/model/dao/IStudent.java
+++ sistemapagosmodel/src/main/java/mx/com/mentoringit/model/dao/IStudent.java
@@ -7,5 +7,7 @@ import mx.com.mentoringit.model.dto.StudentDTO;
 public interface IStudent {
 	
 	public List<StudentDTO> select() throws Exception;
+	public void insertStudent(StudentDTO student) throws Exception;
+	public StudentDTO selectIdMax() throws Exception;
 
 }
--- sistemapagosmodel/src/main/java/mx/com/mentoringit/model/dao/StudentDAO.java
+++ sistemapagosmodel/src/main/java/mx/com/mentoringit/model/dao/StudentDAO.java
@@ -1,11 +1,10 @@
 package mx.com.mentoringit.model.dao;
 
-
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import java.util.List;
-
 
 import javax.sql.DataSource;
 
@@ -14,15 +13,18 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import mx.com.mentoringit.model.dto.PaymentDTO;
 import mx.com.mentoringit.model.dto.StudentDTO;
-
+import mx.com.mentoringit.model.dto.UserDTO;
 
 @Repository
 public class StudentDAO implements IStudent {
-	
-	public StudentDAO(){};
-	
+
+	public StudentDAO() {
+	};
+
 	private JdbcTemplate jdbcTemplate;
+
 	@Autowired
 	public void setJdbcTemplate(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
@@ -31,19 +33,42 @@ public class StudentDAO implements IStudent {
 	public List<StudentDTO> select() throws Exception {
 		String select = "select * from tbl_student";
 		List<StudentDTO> lAlumnos = this.jdbcTemplate.query(select, new RowMapper<StudentDTO>() {
+			public StudentDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+				StudentDTO student = new StudentDTO();
+				student.setId(rs.getInt("ID"));
+				student.setEmail(rs.getString("EMAIL"));
+				student.setName(rs.getString("NAME"));
+				student.setPhone(rs.getString("PHONE"));
+				student.setAddres(rs.getString("ADDRESS"));
+				student.setStatus(rs.getShort("STATUS"));
+				return student;
+			}
+		});
+
+		return lAlumnos;
+	}
+	
+	public StudentDTO selectIdMax() throws Exception {
+		StudentDTO student = this.jdbcTemplate.queryForObject(
+		        "select max(s.id) as ID from tbl_student as s",
+		        new RowMapper<StudentDTO>() {
 		            public StudentDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
-		            	StudentDTO student = new StudentDTO();
-		            	student.setId(rs.getInt("ID"));
-		            	student.setEmail(rs.getString("EMAIL"));
-		            	student.setName(rs.getString("NAME"));
-		            	student.setPhone(rs.getString("PHONE"));
-		            	student.setAddres(rs.getString("ADDRESS"));
-		            	student.setStatus(rs.getShort("STATUS"));
+		                StudentDTO student = new StudentDTO();
+		                student.setId(rs.getInt("ID"));
 		                return student;
 		            }
 		        });
+		return student;
+	}
+
+
+
+	public void insertStudent(StudentDTO student) throws Exception {
+		String insert ="insert into tbl_student(email,name,phone,status) "+ 
+						"values (?,?,?,1)";
 		
-		return lAlumnos;
+		this.jdbcTemplate.update(insert,student.getEmail(),student.getName(),student.getPhone());	
+		
 	}
 
 }
