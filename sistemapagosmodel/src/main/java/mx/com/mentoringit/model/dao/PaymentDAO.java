fb4087e42edd5e7e5280f78c2de150980bf89844
ed <ed@192.168.1.65>
2017-09-06 15:21:29 -0500

    ok

--- sistemapagosmodel/src/main/java/mx/com/mentoringit/model/dao/PaymentDAO.java
+++ sistemapagosmodel/src/main/java/mx/com/mentoringit/model/dao/PaymentDAO.java
@@ -17,54 +17,53 @@ import mx.com.mentoringit.model.dto.PaymentDTO;
 import mx.com.mentoringit.model.dto.StudentDTO;
 
 @Repository
-public class PaymentDAO implements IPayment{
-	
+public class PaymentDAO implements IPayment {
+
 	private JdbcTemplate jdbcTemplate;
-	
+
 	@Autowired
 	public void setJdbcTemplate(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
-	
-	public void insertPayment(PaymentDTO payment) throws Exception {
-		String insert ="insert into tbl_payment " +
-						"(student_id,course_id,num_payment,amount_payment,type_payment,date_payment,total_course,product_id,type_register) " +
-						"values (?,?,?,?,?,?,?,?,?)";
-		
-		Date date = new Date(payment.getDate_payment().getTime());
-		
-		this.jdbcTemplate.update(insert,payment.getStudent_id(),payment.getCourse_id(),
-				payment.getNum_payment(),payment.getAmount_payment(),payment.getType_payment(),date,
-				payment.getTotal_course(),payment.getProduct_id(),payment.getType_register());
-	
-		
-	}
-	
-	public List<PaymentDTO> selectPayment(Integer idStudent, Integer idProduct ) throws Exception {
-		String select = "select * from tbl_payment as p where p.student_id = ? and p.product_id = ?";
-		
-		List<PaymentDTO> payment = this.jdbcTemplate.query(select, new Object[]{idStudent,idProduct}, new RowMapper<PaymentDTO>(){
 
-			public PaymentDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
-				PaymentDTO paymentDTO = new PaymentDTO();
-				
-				paymentDTO.setId(rs.getInt("ID"));
-				paymentDTO.setStudent_id(rs.getInt("STUDENT_ID"));
-				paymentDTO.setCourse_id(rs.getInt("COURSE_ID"));
-				paymentDTO.setNum_payment(rs.getInt("NUM_PAYMENT"));
-				paymentDTO.setAmount_payment(rs.getDouble("AMOUNT_PAYMENT"));
-				paymentDTO.setType_payment(rs.getString("TYPE_PAYMENT"));
-				paymentDTO.setDate_payment(rs.getDate("DATE_PAYMENT"));
-				paymentDTO.setTotal_course(rs.getDouble("TOTAL_COURSE"));
-				paymentDTO.setProduct_id(rs.getInt("PRODUCT_ID"));				
-				
-				return paymentDTO;
-			}
-		});		
-				
+	public void insertPayment(PaymentDTO payment) throws Exception {
+		String insert = "insert into tbl_payment "
+				+ "(student_id,course_id,num_payment,amount_payment,type_payment,date_payment,total_course,product_id,type_register,place_course,time_course) "
+				+ "values (?,?,?,?,?,?,?,?,?,?,?)";
+
+		Date date = new Date(payment.getDate_payment().getTime());
+
+		
+		this.jdbcTemplate.update(insert, payment.getStudent_id(), payment.getCourse_id(), payment.getNum_payment(),
+				payment.getAmount_payment(), payment.getType_payment(), date, payment.getTotal_course(),
+				payment.getProduct_id(), payment.getType_register(), payment.getPlaceCourse(), payment.getTimeCourse());
+
+	}
+
+	public List<PaymentDTO> selectPayment(Integer idStudent, Integer idProduct) throws Exception {
+		String select = "select * from tbl_payment as p where p.student_id = ? and p.product_id = ?";
+
+		List<PaymentDTO> payment = this.jdbcTemplate.query(select, new Object[] { idStudent, idProduct },
+				new RowMapper<PaymentDTO>() {
+
+					public PaymentDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+						PaymentDTO paymentDTO = new PaymentDTO();
+
+						paymentDTO.setId(rs.getInt("ID"));
+						paymentDTO.setStudent_id(rs.getInt("STUDENT_ID"));
+						paymentDTO.setCourse_id(rs.getInt("COURSE_ID"));
+						paymentDTO.setNum_payment(rs.getInt("NUM_PAYMENT"));
+						paymentDTO.setAmount_payment(rs.getDouble("AMOUNT_PAYMENT"));
+						paymentDTO.setType_payment(rs.getString("TYPE_PAYMENT"));
+						paymentDTO.setDate_payment(rs.getDate("DATE_PAYMENT"));
+						paymentDTO.setTotal_course(rs.getDouble("TOTAL_COURSE"));
+						paymentDTO.setProduct_id(rs.getInt("PRODUCT_ID"));
+
+						return paymentDTO;
+					}
+				});
+
 		return payment;
 	}
 
-
-	
 }
--- sistemapagosmodel/src/main/java/mx/com/mentoringit/model/dto/PaymentDTO.java
+++ sistemapagosmodel/src/main/java/mx/com/mentoringit/model/dto/PaymentDTO.java
@@ -13,74 +13,108 @@ public class PaymentDTO {
 	private Double total_course;
 	private Integer product_id;
 	private String type_register;
-	
-	
-	
-	public PaymentDTO() {	
+	private String timeCourse;
+	private String placeCourse;
+
+	public PaymentDTO() {
 		this.num_payment = null;
 		this.total_course = 0.0;
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public Integer getStudent_id() {
 		return student_id;
 	}
+
 	public void setStudent_id(Integer student_id) {
 		this.student_id = student_id;
 	}
+
 	public Integer getCourse_id() {
 		return course_id;
 	}
+
 	public void setCourse_id(Integer course_id) {
 		this.course_id = course_id;
 	}
+
 	public Integer getNum_payment() {
 		return num_payment;
 	}
+
 	public void setNum_payment(Integer num_payment) {
 		this.num_payment = num_payment;
 	}
+
 	public Double getAmount_payment() {
 		return amount_payment;
 	}
+
 	public void setAmount_payment(Double amount_payment) {
 		this.amount_payment = amount_payment;
 	}
+
 	public String getType_payment() {
 		return type_payment;
 	}
+
 	public void setType_payment(String type_payment) {
 		this.type_payment = type_payment;
 	}
-	
+
 	public Double getTotal_course() {
 		return total_course;
 	}
+
 	public void setTotal_course(Double total_course) {
 		this.total_course = total_course;
 	}
+
 	public Integer getProduct_id() {
 		return product_id;
 	}
+
 	public void setProduct_id(Integer product_id) {
 		this.product_id = product_id;
 	}
+
 	public Date getDate_payment() {
 		return date_payment;
 	}
+
 	public void setDate_payment(Date date_payment) {
 		this.date_payment = date_payment;
 	}
+
 	public String getType_register() {
 		return type_register;
 	}
+
 	public void setType_register(String type_register) {
 		this.type_register = type_register;
 	}
-	
+
+	public String getTimeCourse() {
+		return timeCourse;
+	}
+
+	public String getPlaceCourse() {
+		return placeCourse;
+	}
+
+	public void setTimeCourse(String timeCourse) {
+		this.timeCourse = timeCourse;
+	}
+
+	public void setPlaceCourse(String placeCourse) {
+		this.placeCourse = placeCourse;
+	}
 
 }
